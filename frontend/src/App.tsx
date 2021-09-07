363bed0b7ca5fc7ba5593601e864caec134a495c
Bruno A. Padua <bapadua@outlook.com>
2021-09-07 20:48:19 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import DataTable from "components/data-table";
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Semana Dev Superior 4</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Semana Dev Superior 4</h1>
+        <DataTable/>
+      </div>
+      <Footer />
+    </>
   );
 }
 
