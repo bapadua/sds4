b9ffe99de838177ef9d96ccbfd16b8016c43a8e2
Bruno A. Padua <bapadua@outlook.com>
2021-09-07 13:17:05 -0300

    Project Created

---
+++ backend/src/test/java/io/github/bapadua/dsvendas/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bapadua.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
