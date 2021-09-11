d96cebfdd188950660065585c4778376743ba044
Bruno A. Padua <bapadua@outlook.com>
2021-09-11 20:27:38 -0300

    estilo modificado

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -16,7 +16,7 @@ function Dashboard() {
                         <BarChart />
                     </div>
                     <div className="col-sm-6">
-                        <h5>Todas as vendas</h5>
+                        <h5 className="text-center text-secondary">Todas as vendas</h5>
                         <DonutChart />
                     </div>
                 </div>
