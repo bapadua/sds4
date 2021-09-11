3ac10919dd687d2de446e4cb77329d7f44d10833
Bruno A. Padua <bapadua@outlook.com>
2021-09-11 17:17:07 -0300

    BarChart integration

--- frontend/src/components/barchart/index.tsx
+++ frontend/src/components/barchart/index.tsx
@@ -1,6 +1,52 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { BASE_URL } from 'utils/requests';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/math-round';
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+};
 
 function BarChart() {
+
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [{
+            name: '',
+            data: []
+        }]
+    });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSuccess[];
+                const dataLabels = data.map(sale => sale.sellerName);
+                const dataSeries = data.map(sale => getPercent(sale.deals, sale.visited));
+
+                setChartData({
+                    labels: {
+                        categories: dataLabels
+                    },
+                    series: [{
+                        name: '% sucesso',
+                        data: dataSeries
+                    }]
+                });
+            })
+    }, []);
 
     const options = {
         plotOptions: {
@@ -10,26 +56,18 @@ function BarChart() {
         },
     };
 
-    const mockData = {
-        labels: {
-            categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [43.6, 67.1, 67.7, 45.6, 71.1]
-            }
-        ]
-    };
-
     return (
         <Chart
-            options={{ ...options, xaxis: mockData.labels }}
-            series={mockData.series}
+            options={{ ...options, xaxis: chartData.labels }}
+            series={chartData.series}
             type="bar"
             height="240"
         />
     );
+}
+
+function getPercent(deals: number, visited: number) {
+    return round(100.0 * deals / visited, 1);
 }
 
 export default BarChart;
